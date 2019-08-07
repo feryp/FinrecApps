1c635aec99063941aeb929052a03b79c42530168
ferrtama <fery.pratama20@gmail.com>
2019-08-07 18:56:28 +0700

    Merge branch 'master' of C:\Users\FERY\AndroidStudioProjects\FinrecApps with conflicts.

--- app/src/main/java/com/example/finrecapps/MainActivity.java
+++ app/src/main/java/com/example/finrecapps/MainActivity.java
@@ -1,22 +1,15 @@
 package com.example.finrecapps;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout periode_bulan, periode_tahun;
-
-    TextView tvLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
         periode_bulan = findViewById(R.id.container_bulan);
         periode_tahun = findViewById(R.id.container_tahun);
-        tvLogout = findViewById(R.id.tv_logout);
 
 
 
@@ -45,24 +37,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        tvLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences pref = getSharedPreferences(getString(R.string.shared_pref_password), Context.MODE_PRIVATE);
-                SharedPreferences.Editor edit = pref.edit();
-
-                edit.putBoolean("isLogin", false);
-
-                edit.apply();
-
-                Intent i = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(i);
-                finish();
-            }
-        });
-
 
     }
-
-
 }
